86eb7044dec92746d1618ac8d0c59d72bac80253
ChrisJP84 <christopher.jprice@outlook.com>
2025-05-28 07:19:42 +0000

    file_io: futher refactoring of console output

--- file_io_lib/src/file_io.c
+++ file_io_lib/src/file_io.c
@@ -9,7 +9,7 @@ bool load_state(uint8_t *state, uint32_t num_bytes)
 {
     if (state == NULL || num_bytes == 0)
     {
-        printf("load_state: bad arguments\n");
+        printf("file_io:load_state: bad arguments, failed\n");
         return false;
     }
 
@@ -17,20 +17,20 @@ bool load_state(uint8_t *state, uint32_t num_bytes)
 
     if (f == NULL)
     {
-        printf("load_state: fopen failed\n");
+        printf("file_io:load_state: fopen failed\n");
         return false;
     }
 
     if (fread(state, 1, num_bytes, f) != (size_t)num_bytes)
     {
-        printf("load_state: fread failed\n");
+        printf("file_io:load_state: fread failed\n");
         fclose(f);
         return false;
     }
 
     if (fclose(f) != 0)
     {
-        printf("load_state: fclose failed\n");
+        printf("file_io:load_state: fclose failed\n");
         return false;
     }
 
@@ -41,7 +41,7 @@ bool save_state(uint8_t *state, uint32_t num_bytes)
 {
     if (state == NULL || num_bytes == 0)
     {
-        printf("save_state: no cached data\n");
+        printf("file_io:save_state: bad arguments, failed\n");
         return false;
     }
 
@@ -49,14 +49,14 @@ bool save_state(uint8_t *state, uint32_t num_bytes)
 
     if (f == NULL)
     {
-        printf("save_state: fopen failed\n");
+        printf("file_io:save_state: fopen failed\n");
         return false;
     }
 
     // Write state to file
     if (fwrite(state, 1, num_bytes, f) != (size_t)num_bytes)
     {
-        printf("save_state: fwrite failed\n");
+        printf("file_io:save_state: fwrite failed\n");
         fclose(f);
         return false;
     }
@@ -64,14 +64,14 @@ bool save_state(uint8_t *state, uint32_t num_bytes)
     // force flush
     if (fflush(f) != 0)
     {
-        printf("save_state: fflush failed\n");
+        printf("file_io:save_state: fflush failed\n");
         fclose(f);
         return false;
     }
 
     if (fclose(f) != 0)
     {
-        printf("save_state: fclose failed\n");
+        printf("file_io:save_state: fclose failed\n");
         return false;
     }
 
